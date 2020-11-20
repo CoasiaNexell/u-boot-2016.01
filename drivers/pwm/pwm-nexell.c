5a2d1ac8921241c144234b48a96c2650adffcff2
GyoungBo Min <mingyoungbo@coasianexell.com>
2020-11-20 10:47:22 +0900

    u-boot: pwm: bug fix modify pwm period
    This patch modify pwm driver for wrong pwm peroid.
    
    Signed-off-by: GyoungBo Min <mingyoungbo@coasianexell.com>
    Change-Id: If0133739c3aa2245915f7869d77050bebfd8ccb6

--- drivers/pwm/pwm-nexell.c
+++ drivers/pwm/pwm-nexell.c
@@ -70,51 +70,6 @@ void pwm_disable(int pwm_id)
 	writel(tcon, &pwm->tcon);
 }
 
-static unsigned long pwm_calc_tin(int pwm_id, unsigned long freq)
-{
-	unsigned long tin_parent_rate;
-	unsigned int div;
-#if defined(CONFIG_ARCH_NEXELL)
-	unsigned int pre_div;
-	const struct s5p_timer *pwm =
-		(struct s5p_timer *)PHY_BASEADDR_PWM;
-	unsigned int val;
-#endif
-
-#if defined(CONFIG_ARCH_NEXELL)
-	struct clk *clk = clk_get(CORECLK_NAME_PCLK);
-	tin_parent_rate = clk_get_rate(clk);
-#else
-	tin_parent_rate = get_pwm_clk();
-#endif
-
-#if defined(CONFIG_ARCH_NEXELL)
-	writel(0, &pwm->tcfg0);
-	val = readl(&pwm->tcfg0);
-
-	if (pwm_id < 2)
-		div = ((val >> 0) & 0xff) + 1;
-	else
-		div = ((val >> 8) & 0xff) + 1;
-
-	writel(0, &pwm->tcfg1);
-	val = readl(&pwm->tcfg1);
-	val = (val >> MUX_DIV_SHIFT(pwm_id)) & 0xF;
-	pre_div = (1UL << val);
-
-	freq = tin_parent_rate / div / pre_div;
-
-	return freq;
-#else
-	for (div = 2; div <= 16; div *= 2) {
-		if ((tin_parent_rate / (div << 16)) < freq)
-			return tin_parent_rate / div;
-	}
-
-	return tin_parent_rate / 16;
-#endif
-}
-
 #define NS_IN_SEC 1000000000UL
 
 int pwm_config(int pwm_id, int duty_ns, int period_ns)
@@ -126,12 +81,14 @@ int pwm_config(int pwm_id, int duty_ns, int period_ns)
 		(struct s5p_timer *)samsung_get_base_timer();
 #endif
 	unsigned int offset;
-	unsigned long tin_rate;
 	unsigned long tin_ns;
-	unsigned long frequency;
 	unsigned long tcon;
 	unsigned long tcnt;
 	unsigned long tcmp;
+	u32 val;
+	unsigned int prescaler;
+	uint div, rate;
+	struct clk *clk = clk_get(CORECLK_NAME_PCLK);
 
 	/*
 	 * We currently avoid using 64bit arithmetic by using the
@@ -144,22 +101,16 @@ int pwm_config(int pwm_id, int duty_ns, int period_ns)
 	if (duty_ns > period_ns)
 		return -EINVAL;
 
-	frequency = NS_IN_SEC / period_ns;
+	val = readl(&pwm->tcfg0);
+	prescaler = (pwm_id < 2 ? val : (val >> 8)) & 0xff;
+	div = (readl(&pwm->tcfg1) >> MUX_DIV_SHIFT(pwm_id)) & 0xf;
 
-	/* Check to see if we are changing the clock rate of the PWM */
-	tin_rate = pwm_calc_tin(pwm_id, frequency);
+	rate = clk_get_rate(clk) / ((prescaler + 1) * (1 << div));
+	debug("[%s:%d] rate %u\n", __func__, __LINE__, rate);
 
-	tin_ns = NS_IN_SEC / tin_rate;
-#if defined(CONFIG_ARCH_NEXELL)
-	/* The counter starts at zero. */
-	tcnt = (period_ns / tin_ns) - 1;
-#else
+	tin_ns = 1000000000 / rate;
 	tcnt = period_ns / tin_ns;
-#endif
-
-	/* Note, counters count down */
 	tcmp = duty_ns / tin_ns;
-	tcmp = tcnt - tcmp;
 
 	/* Update the PWM register block. */
 	offset = pwm_id * 3;
@@ -178,6 +129,8 @@ int pwm_config(int pwm_id, int duty_ns, int period_ns)
 
 	tcon &= ~TCON_UPDATE(pwm_id);
 	writel(tcon, &pwm->tcon);
+
+	pwm_enable(pwm_id);
 
 	return 0;
 }
@@ -215,30 +168,6 @@ int pwm_init(int pwm_id, int div, int invert)
 	val |= (div & 0xf) << MUX_DIV_SHIFT(pwm_id);
 	writel(val, &pwm->tcfg1);
 
-	if (pwm_id == 4) {
-		/*
-		 * TODO(sjg): Use this as a countdown timer for now. We count
-		 * down from the maximum value to 0, then reset.
-		 */
-		ticks_per_period = -1UL;
-	} else {
-		const unsigned long pwm_hz = 1000;
-#if defined(CONFIG_ARCH_NEXELL)
-		struct clk *clk = clk_get(CORECLK_NAME_PCLK);
-		unsigned long timer_rate_hz = clk_get_rate(clk) /
-#else
-		unsigned long timer_rate_hz = get_pwm_clk() /
-#endif
-			((prescaler + 1) * (1 << div));
-
-		ticks_per_period = timer_rate_hz / pwm_hz;
-	}
-
-	/* set count value */
-	offset = pwm_id * 3;
-
-	writel(ticks_per_period, &pwm->tcntb0 + offset);
-
 	val = readl(&pwm->tcon) & ~(0xf << TCON_OFFSET(pwm_id));
 	if (invert && (pwm_id < 4))
 		val |= TCON_INVERTER(pwm_id);
@@ -246,7 +175,6 @@ int pwm_init(int pwm_id, int div, int invert)
 
 	nx_gpio_set_pad_function(pwm_dev[pwm_id].grp, pwm_dev[pwm_id].bit,
 				 pwm_dev[pwm_id].pwm_fn);
-	pwm_enable(pwm_id);
 
 	return 0;
 }
